8c4bd47b3949e2eb2e9a5838712cf8b97dcd6278
AbyssGeoV <hhartmann@geovictoria.com>
2022-11-25 09:03:49 -0300

    init commit

---
+++ RequestGeneralApi/Helpers/RetryHelper.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading;
+
+namespace Helpers
+{
+    public static class RetryHelper
+    {
+        private const string DEFAULT_TOTAL_ATTEMPTS = "2";
+
+        private static readonly string TOTAL_ATTEMPTS = DEFAULT_TOTAL_ATTEMPTS;
+        public static T Execute<T>(Func<T> method)
+        {
+            int currentRetry = 1;
+
+            T value = default(T);
+
+            for (; ; )
+            {
+                try
+                {
+                    value = method();
+                    break;
+                }
+                catch (Exception)
+                {
+                    currentRetry++;
+                    if (currentRetry > int.Parse(TOTAL_ATTEMPTS))
+                    {
+                        throw;
+                    }
+                }
+                Thread.Sleep(50);
+            }
+
+            return value;
+
+        }
+
+        public static void Execute(Action method)
+        {
+            int currentRetry = 1;
+
+            for (; ; )
+            {
+                try
+                {
+                    method();
+                    break;
+                }
+                catch (Exception)
+                {
+                    currentRetry++;
+                    if (currentRetry > int.Parse(TOTAL_ATTEMPTS))
+                    {
+                        throw;
+                    }
+                }
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
