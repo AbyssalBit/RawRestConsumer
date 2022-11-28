3b2cec7664ea456d2a0e3b3087f3584425312b05
AbyssalBit <hrhartmann@uc.cl>
2022-11-28 17:42:23 -0300

    init: configs and refactor

--- Program.cs
+++ Program.cs
@@ -3,31 +3,75 @@ using System;
 using System.Net.Http;
 using System.Text;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace RequestGeneralApi
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static readonly string fullUri = "http://127.0.0.1:8000/testpost";
+        private static readonly double TimeOut = 1;
+        static async Task Main(string[] args)
         {
+            // string header = @"{
+            //                 { 'X-RapidAPI-Key', '086aae3746mshdb3f3ea0c68f17ap18bd08jsnbfa1b20c2539' },
+            //                 { 'X-RapidAPI-Host', 'google-translate1.p.rapidapi.com' },
+            //                 }";
+
+
+            // Dictionary<string, string> Headers = {
+            //                 { "X-RapidAPI-Key", "086aae3746mshdb3f3ea0c68f17ap18bd08jsnbfa1b20c2539" },
+            //                 { 'X-RapidAPI-Host', 'google-translate1.p.rapidapi.com' },
+            //                 };
+
+
+            Dictionary<string, string> headersDict = new Dictionary<string, string>();
+            headersDict.Add("X-RapidAPI-Key", "086aae3746mshdb3f3ea0c68f17ap18bd08jsnbfa1b20c2539");
+            headersDict.Add("X-RapidAPI-Host", "google-translate1.p.rapidapi.com");
+            string content = @"{
+                { 'q', 'English is hard, but detectably so' },
+            }";
+            
+            await Request(fullUri, headersDict, content);
+
+        }
+        static async Task Request(string apiUri, Dictionary<string, string> headersDict, string content) 
+        {
+            
             var client = new HttpClient();
+            // client.DefaultRequestHeaders = new StringContent(header, Encoding.UTF8, "application/json");
+            // client.DefaultRequestHeaders.Add("ApiKey", key);
+            foreach (KeyValuePair<string, string> element in headersDict)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", element.Key, element.Value);
+                client.DefaultRequestHeaders.Add(element.Key, element.Value);
+            }
+            var json = JsonConvert.SerializeObject(content);
             var request = new HttpRequestMessage
             {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete?query=%3CREQUIRED%3E"),
-                Headers =
-                {
-                    { "X-RapidAPI-Key", "086aae3746mshdb3f3ea0c68f17ap18bd08jsnbfa1b20c2539" },
-                    { "X-RapidAPI-Host", "bloomberg-market-and-financial-news.p.rapidapi.com" },
-                },
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(apiUri),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
             };
-            using (var response = await client.SendAsync(request))
-            {
-                response.EnsureSuccessStatusCode();
-                var body = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(body);
-            }
+            // request.Headers = new StringContent(header, Encoding.UTF8, "application/json");
+
+
+
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+
+            var deserialized_response = JsonConvert.DeserializeObject<dynamic>(body);
+            string ans = (string)(deserialized_response);
+
+            Console.WriteLine(ans);
+
+
+
+        
+
         }
     }
 }
--- RawRestConsumer.cs
+++ RawRestConsumer.cs
@@ -1,135 +1,135 @@
-using Newtonsoft.Json;
-using System;
-using System.Net.Http;
-using System.Text;
-using System.Linq;
-using System.Text.RegularExpressions;
-using Helpers;
+// // using Newtonsoft.Json;
+// using System;
+// using System.Net.Http;
+// using System.Text;
+// using System.Linq;
+// using System.Text.RegularExpressions;
+// using Helpers;
 
-namespace RequestGeneralApi
-{
-    internal sealed class RawRestConsumer
-        {
-            private static readonly double TimeOut = "1";
-            private static readonly string BaseApiUrl = "";
-            private static readonly string AuthorizationKey = "";
-            private readonly HttpClient Client = null;
-            // private static readonly HttpClient MotorCalculoRestClient = null;
-            // private static readonly HttpClient RawDataRestClient = null;
+// namespace RequestGeneralApi
+// {
+//     internal sealed class RawRestConsumer
+//         {
+//             private static readonly double TimeOut = "1";
+//             private static readonly string BaseApiUrl = "";
+//             private static readonly string AuthorizationKey = "";
+//             private readonly HttpClient Client = null;
+//             // private static readonly HttpClient MotorCalculoRestClient = null;
+//             // private static readonly HttpClient RawDataRestClient = null;
 
-            static RestConsumer()
-                {
-                    Client = new HttpClient
-                        {
-                            BaseAddress = new Uri(BaseApiUrl),
-                            Timeout = TimeSpan.FromMinutes(TimeOut)
-                        };
-            }
-            public RestConsumer(string ApiUrl, string AuthorizationKey)
-            {
-                Client = new HttpClient
-                    {
-                        BaseAddress = new Uri(BaseApiUrl),
-                        Timeout = TimeSpan.FromMinutes(TimeOut)
-                    };
-                Client.DefaultRequestHeaders.Add("authorization", AuthorizationKey);
+//             static RawRestConsumer()
+//                 {
+//                     Client = new HttpClient
+//                         {
+//                             BaseAddress = new Uri(BaseApiUrl),
+//                             Timeout = TimeSpan.FromMinutes(TimeOut)
+//                         };
+//             }
+//             public RawRestConsumer(string ApiUrl, string AuthorizationKey)
+//             {
+//                 Client = new HttpClient
+//                     {
+//                         BaseAddress = new Uri(BaseApiUrl),
+//                         Timeout = TimeSpan.FromMinutes(TimeOut)
+//                     };
+//                 Client.DefaultRequestHeaders.Add("authorization", AuthorizationKey);
                 
-            }
-            public T GetResponse<T, U>(string url, U obj)
-            {
-                T ret = default;
+//             }
+//             public T GetResponse<T, U>(string url, U obj)
+//             {
+//                 T ret = default;
 
-                HttpResponseMessage response = null;
+//                 HttpResponseMessage response = null;
 
-                RetryHelper.Execute(
-                    () =>
-                    {
-                        response = AsyncHelper.RunSync<HttpResponseMessage>(() => Client.PostAsync(url, CreateHttpContent<U>(obj)));
-                        response.EnsureSuccessStatusCode();
-                    }
-                );
-
-
-                Console.WriteLine(response);
-
-                return ret;
-            }
-
-            public string GetStringResponse<T>(string url, T obj)
-            {
+//                 RetryHelper.Execute(
+//                     () =>
+//                     {
+//                         response = AsyncHelper.RunSync<HttpResponseMessage>(() => Client.PostAsync(url, CreateHttpContent<U>(obj)));
+//                         response.EnsureSuccessStatusCode();
+//                     }
+//                 );
 
 
-                HttpResponseMessage response = null;
+//                 Console.WriteLine(response);
 
-                RetryHelper.Execute(
-                    () =>
-                    {
-                        response = AsyncHelper.RunSync<HttpResponseMessage>(() => Client.PostAsync(url, CreateHttpContent<T>(obj)));
-                        response.EnsureSuccessStatusCode();
-                    }
-                );
+//                 return ret;
+//             }
+
+//             public string GetStringResponse<T>(string url, T obj)
+//             {
 
 
-                if (response.IsSuccessStatusCode)
-                {
-                    string resp = AsyncHelper.RunSync<string>(() => response.Content.ReadAsStringAsync());
+//                 HttpResponseMessage response = null;
 
-                    dynamic stuff = JsonConvert.DeserializeObject<dynamic>(resp);
-
-
-                    return resp;
-                    // return JsonConvert.DeserializeObject<T>(resp);
-                }
-
-                return "error";
-                //return ret;
-            }
-
-            private static void bossaIterate(dynamic variable, int indent)
-            {
+//                 RetryHelper.Execute(
+//                     () =>
+//                     {
+//                         response = AsyncHelper.RunSync<HttpResponseMessage>(() => Client.PostAsync(url, CreateHttpContent<T>(obj)));
+//                         response.EnsureSuccessStatusCode();
+//                     }
+//                 );
 
 
-                if (variable.GetType() == typeof(Newtonsoft.Json.Linq.JObject))
-                {
+//                 if (response.IsSuccessStatusCode)
+//                 {
+//                     string resp = AsyncHelper.RunSync<string>(() => response.Content.ReadAsStringAsync());
 
-                    foreach (var property in variable)
-                    {
-                        for (int i = 0; i < indent; i++)
-                        {
-                            System.Diagnostics.Debug.Write("_");
-                        }
+//                     dynamic stuff = JsonConvert.DeserializeObject<dynamic>(resp);
 
 
-                        bossaIterate(property.Value, indent + 1);
+//                     return resp;
+//                     // return JsonConvert.DeserializeObject<T>(resp);
+//                 }
 
-                    }
-                }
-                else if (variable.GetType() == typeof(Newtonsoft.Json.Linq.JArray))
-                {
+//                 return "error";
+//                 //return ret;
+//             }
 
-                    System.Diagnostics.Debug.WriteLine("");
-                    foreach (var item in variable)
-                    {
-                        for (int i = 0; i < indent; i++)
-                        {
-                            System.Diagnostics.Debug.Write("_");
-                        }
-                        bossaIterate(item, indent + 1);
-                    }
-                }
-                else if (variable.GetType() == typeof(Newtonsoft.Json.Linq.JValue))
-                {
-                    //System.Diagnostics.Debug.Write("#"+variable.ToString()+"#");
+//             private static void bossaIterate(dynamic variable, int indent)
+//             {
 
-                    //Console.WriteLine("type is Variable, value: " + variable.ToString());
-                }
-            }
-            private HttpContent CreateHttpContent<T>(T content)
-            {
-                var json = JsonConvert.SerializeObject(content);
-                return new StringContent(json, Encoding.UTF8, "application/json");
-            }
 
-        }
+//                 if (variable.GetType() == typeof(Newtonsoft.Json.Linq.JObject))
+//                 {
+
+//                     foreach (var property in variable)
+//                     {
+//                         for (int i = 0; i < indent; i++)
+//                         {
+//                             System.Diagnostics.Debug.Write("_");
+//                         }
+
+
+//                         bossaIterate(property.Value, indent + 1);
+
+//                     }
+//                 }
+//                 else if (variable.GetType() == typeof(Newtonsoft.Json.Linq.JArray))
+//                 {
+
+//                     System.Diagnostics.Debug.WriteLine("");
+//                     foreach (var item in variable)
+//                     {
+//                         for (int i = 0; i < indent; i++)
+//                         {
+//                             System.Diagnostics.Debug.Write("_");
+//                         }
+//                         bossaIterate(item, indent + 1);
+//                     }
+//                 }
+//                 else if (variable.GetType() == typeof(Newtonsoft.Json.Linq.JValue))
+//                 {
+//                     //System.Diagnostics.Debug.Write("#"+variable.ToString()+"#");
+
+//                     //Console.WriteLine("type is Variable, value: " + variable.ToString());
+//                 }
+//             }
+//             private HttpContent CreateHttpContent<T>(T content)
+//             {
+//                 var json = JsonConvert.SerializeObject(content);
+//                 return new StringContent(json, Encoding.UTF8, "application/json");
+//             }
+
+//         }
     
-    }
+//     }
