88f2138e843c41c7f50b4292374f31190acfb24b
AbyssGeoV <hhartmann@geovictoria.com>
2022-12-06 18:05:07 -0300

    feats: steel python

--- Program.cs
+++ Program.cs
@@ -6,72 +6,56 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using SteelPython;
 
 namespace RequestGeneralApi
 {
     internal class Program
     {
-        private static readonly string fullUri = "http://127.0.0.1:8000/testpost";
+        private static readonly string fullUri = "https://devmotorgv.azurewebsites.net/api/turno";
         private static readonly double TimeOut = 1;
         static async Task Main(string[] args)
         {
-            // string header = @"{
-            //                 { 'X-RapidAPI-Key', '086aae3746mshdb3f3ea0c68f17ap18bd08jsnbfa1b20c2539' },
-            //                 { 'X-RapidAPI-Host', 'google-translate1.p.rapidapi.com' },
-            //                 }";
-
-
-            // Dictionary<string, string> Headers = {
-            //                 { "X-RapidAPI-Key", "086aae3746mshdb3f3ea0c68f17ap18bd08jsnbfa1b20c2539" },
-            //                 { 'X-RapidAPI-Host', 'google-translate1.p.rapidapi.com' },
-            //                 };
-
 
             Dictionary<string, string> headersDict = new Dictionary<string, string>();
-            headersDict.Add("X-RapidAPI-Key", "086aae3746mshdb3f3ea0c68f17ap18bd08jsnbfa1b20c2539");
-            headersDict.Add("X-RapidAPI-Host", "google-translate1.p.rapidapi.com");
-            string content = @"{
-                { 'q', 'English is hard, but detectably so' },
-            }";
+            headersDict.Add("authorization", "no");
+            // headersDict.Add("Content-type", "application/json");
+            string content = "{\"IdEmpresa\": 5702}";
+            // CustomPythonEngine _customPythonEngine = new CustomPythonEngine();
+            CustomPythonEngine.RunPython();
             
-            await Request(fullUri, headersDict, content);
+            // await Request(fullUri, headersDict, content);
+
+
 
         }
         static async Task Request(string apiUri, Dictionary<string, string> headersDict, string content) 
         {
-            
             var client = new HttpClient();
             // client.DefaultRequestHeaders = new StringContent(header, Encoding.UTF8, "application/json");
             // client.DefaultRequestHeaders.Add("ApiKey", key);
             foreach (KeyValuePair<string, string> element in headersDict)
             {
-                Console.WriteLine("Key: {0}, Value: {1}", element.Key, element.Value);
+                // Console.WriteLine("Key: {0}, Value: {1}", element.Key, element.Value);
                 client.DefaultRequestHeaders.Add(element.Key, element.Value);
             }
-            var json = JsonConvert.SerializeObject(content);
+            // var json = JsonConvert.SerializeObject(content);
+
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Post,
                 RequestUri = new Uri(apiUri),
-                Content = new StringContent(json, Encoding.UTF8, "application/json")
+                Content = new StringContent(content, Encoding.UTF8, "application/json")
             };
-            // request.Headers = new StringContent(header, Encoding.UTF8, "application/json");
-
-
 
             var response = await client.SendAsync(request);
             response.EnsureSuccessStatusCode();
             var body = await response.Content.ReadAsStringAsync();
-
-            var deserialized_response = JsonConvert.DeserializeObject<dynamic>(body);
-            string ans = (string)(deserialized_response);
-
-            Console.WriteLine(ans);
-
-
-
-        
-
+            // Console.WriteLine(body);
+            // var deserialized_response = JsonConvert.DeserializeObject<dynamic>(body);
+            dynamic stuff = JsonConvert.DeserializeObject<dynamic>(body);
+            // string ans = (string)(deserialized_response);
+            Console.WriteLine(stuff);
         }
     }
 }
